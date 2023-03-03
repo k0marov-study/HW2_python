edb3226f5963a39f7e4ffc6c076a1fa6d29f5ea3
Sam Komarov <sam@skomarov.com>
2023-03-03 19:59:19 +0300

    solve task #5

---
+++ task_5.py
@@ -0,0 +1,17 @@
+a = [] 
+x = int(input()) 
+while x != 0: 
+    a.append(x) 
+    x = int(input()) 
+
+max_elem = 0 
+for x in a: 
+    if x > max_elem: 
+        max_elem = x 
+
+equal_max_cnt = 0 
+for x in a: 
+    if x == max_elem: 
+        equal_max_cnt += 1
+
+print(equal_max_cnt)
